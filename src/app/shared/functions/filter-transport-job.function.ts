3030a0ce6e3330b1b7c827c639690b395fff8498
WIN-GU26UEIA5T0\user <artur.yedigaryan13@gmail.com>
2026-05-28 13:58:52 +0400

    fix issue

--- src/app/shared/functions/filter-transport-job.function.ts
+++ src/app/shared/functions/filter-transport-job.function.ts
@@ -2,8 +2,11 @@ import { TransportJob } from '../interfaces/truck/transport-job.interface';
 
 export function transportJobsFilter(searchString: string, jobs: TransportJob[]) {
   const search = searchString?.toLowerCase().trim();
-  if (!search) return jobs;
-  return jobs.filter((job) =>
-    job.cargos.some((cargo) => cargo.description?.toLowerCase().includes(search)),
+  if (!search) return jobs ? [...jobs] : [];
+
+  return (jobs ?? []).filter(job =>
+    job.cargos?.some(cargo =>
+      cargo.description?.toLowerCase().includes(search),
+    ),
   );
 }
